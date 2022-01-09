2c0d029001bf843cc971ccc71fc81fa4a188bbc8
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-09 21:32:38 +0900

    [Update] - Updated Codes

--- Loop/Code11721.kt
+++ Loop/Code11721.kt
@@ -7,6 +7,8 @@ fun main(){
     for (i in newStringLine.indices){
         val b = newStringLine.substring(i, i + 1);
         
+        // If No.9 index (No.9, No.19, No.29, ...) of Items 
+        // matches to rounded-value
         if(i % 10 == 9) println(b);
         else print(b);
     }
