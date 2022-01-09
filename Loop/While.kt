756f56b0f686ae7e9229bc57890915b589993507
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-09 20:50:26 +0900

    [Feature] - Items with Array and ArraySize

--- Loop/While.kt
+++ Loop/While.kt
@@ -1,10 +1,23 @@
-fun whileFun(){
+fun main(){
     println("For Loop")
 
     val items = listOf("Apple", "Banana", "Wiki")
-    println(items)
-
+    println(items.size)
+    println(items + "\n")
+    
+    println("<Items with `General For-Loop>`")
     for(item in items){
-        println(item)
+        print("$item ")
     }
+
+    println("\n\n--------------------\n")
+
+    println("<Items with `Array` and `Array.size`>")
+    for(index in 0..(items.size - 1)){
+        print("${items[index]} ")
+    }
+    
+    println("\n\n--------------------\n")
+
+    println("<>")
 }
