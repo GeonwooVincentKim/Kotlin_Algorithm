31a64b92151c2d9756754e27f587421c192eb917
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-09 21:01:42 +0900

    [Feature & Update] - Show the result of ArraySize Loop

--- Loop/While.kt
+++ Loop/While.kt
@@ -16,6 +16,17 @@ fun main(){
     for(index in 0..(items.size - 1)){
         print("${items[index]} ")
     }
+
+    /*
+        Occurs `Array Index out of Bounds Exception`
+        Because the size of Array equals to 3,
+        but it trying to find out the No.3 Index,
+        so it occurs error that there is no Index 
+        that you looking for
+    */ 
+    // for(index in 1..(items.size)){
+    //     print(items[index])
+    // }
     
     println("\n\n--------------------\n")
 
