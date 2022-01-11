5b5df705e3dd38ca0e0a0e4aeb5ba935ae57c627
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-11 20:19:56 +0900

    [Feature] - Added Code1924.kt file

---
+++ Loop/Code1924.kt
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+fun main(){
+    var sc: Scanner = Scanner(System.`in`);
+
+    var a = sc.nextInt()
+    var b = sc.nextInt()
+
+    var sum = 0;
+
+    var months = arrayOf(0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+
+    for(i in 1..12){
+        if (i == a){
+            break;
+        }
+        sum += months.get(i)
+    }
+
+    sum += b;
+
+    var left = sum % 7;
+    when(left) {
+        0 -> println("SUM");
+        1 -> println("MON");
+        2 -> println("TUE");
+        3 -> println("WED");
+        4 -> println("THU");
+        5 -> println("FRI");
+        6 -> println("SAT");
+    }    
+}
