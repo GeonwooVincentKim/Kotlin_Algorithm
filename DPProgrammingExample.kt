e78a88a2011f6763ed20c41de0c84a5c6b30ab2b
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-15 22:51:59 +0900

    [Feature] - Making the example of DPProgramming

--- DPProgrammingExample.kt
+++ DPProgrammingExample.kt
@@ -1,6 +1,12 @@
-import java.util.Scanner;
- 
+import java.util.Scanner; 
+
+var fibonacciData = arrayOf(0, );
 fun fibonacci(n: Int) : Int{
+    fibonacciData[0] = 0;
+    for(i in 1..fibonacciData.size){
+        print(fibonacciData.get(i));
+    }
+
     if (n <= 2) return 1;
     return fibonacci(n - 2) + fibonacci(n - 1)
 }
@@ -9,5 +15,6 @@ fun main(){
     var sc: Scanner = Scanner(System.`in`);
     var a = sc.nextInt();
 
+    // var fibonacciData = arrayOfNulls<Int>(100)
     println(fibonacci(a));
 }
