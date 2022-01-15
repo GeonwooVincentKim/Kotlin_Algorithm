6044cfdd16ab2c54fe599968a0f41e2555d0f2e9
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-15 22:16:41 +0900

    [Feature] - Code1008

---
+++ If/Code1008.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+fun main(){
+    var sc: Scanner = Scanner(System.`in`);
+    var a = sc.nextInt();
+    var b = sc.nextInt();
+
+    var a2 = a.toDouble()
+    var b2 = b.toDouble()
+    println(a2 / b2);
+    // println((a / b))
+}
