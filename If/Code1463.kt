915cdd8628b0f3cc648f6d309f644ad2cc3b441c
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-15 21:12:53 +0900

    [Feature] - Added Code1463.kt

---
+++ If/Code1463.kt
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+fun main(){
+    var sc: Scanner = Scanner(System.`in`);
+    var a = sc.nextInt();
+
+    var count = 0;
+    while(a != 1){
+        if(a == 1){
+            print(count);
+            break;
+        }
+
+        if(a % 3 == 0){
+            count++;
+            a = a / 3
+
+            if(a == 1){
+                print(count);
+                break;
+            }
+        }
+        if(a % 2 == 0){
+            count++;
+            a = a / 2
+
+            if(a == 1){
+                print(count);
+                break;
+            }
+        }
+        if(a - 1 == 1){
+            count++;
+            a -= 1
+
+            if(a == 1){
+                print(count);
+                break;
+            }
+        }
+    }
+}
