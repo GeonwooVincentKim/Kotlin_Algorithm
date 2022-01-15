56dfe1f5e99f89641b206961c6c2dd0120a4f0a6
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-15 22:51:35 +0900

    [Update] - Code1312.kt

--- If/Code1312.kt
+++ If/Code1312.kt
@@ -7,25 +7,9 @@ fun main(){
     var b = sc.nextInt();
     var c = sc.nextInt();
 
-    // a.toDouble();
-    // b.toDouble();
     a %= b    
     for(i in 1..(c - 1)) {
         a = (a * 10) % b
     }
     println((a * 10) / b)
-    // var getValue = a.toDouble() % b.toDouble()
-    // println(getValue);
-    // var getValue2 = getValue.toString()
-
-    // for(i in 1..getValue2.length){
-    //     var getNum = getValue2.split("")
-        
-    //     if(getNum.get(i) == "."){
-
-    //     }
-    //     println(getNum.get(i + 2));
-    // }
-
-    // println(getValue);
 }
