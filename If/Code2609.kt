3be2bba82c19c8a0e0a62b036b1f91c755e87b6d
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-16 20:24:41 +0900

    [Feature] - Code2609

--- If/Code2609.kt
+++ If/Code2609.kt
@@ -1,20 +1,17 @@
 import java.util.Scanner;
 
+fun getGCP(a: Int, b: Int): Int = if (b == 0) a else getGCP(b, a % b)
+fun getLCM(a: Int, b: Int): Int = a * b / getGCP(a, b)
+
 fun main(){
     var sc: Scanner = Scanner(System.`in`);
 
     var a = sc.nextInt();
     var b = sc.nextInt();
 
-    for(i in 1..a){
-        if(a % i == 0){
-            println(i);
-        }
-    }
+    val gcp = getGCP(a, b)
+    val lcm = getLCM(a, b)
 
-    for(j in 1..b){
-        if(b % j == 0){
-            println(j);
-        }
-    }
+    println(gcp)
+    println(lcm)
 }
