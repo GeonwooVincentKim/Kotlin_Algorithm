447e1bb8f7b67ff6d3ffc56ad1d8e98f6b18c7a0
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-20 21:53:55 +0900

    [Feature] - Code15552

--- Loop/Code15552.kt
+++ Loop/Code15552.kt
@@ -1,8 +1,17 @@
 import java.io.*;
+import java.util.StringTokenizer
 
 fun main(){
     val br = BufferedReader(InputStreamReader(System.`in`));
     val bw = BufferedWriter(OutputStreamWriter(System.out));
-    println(br);
-    println(bw);
+    repeat(br.readLine().toInt()){
+        val token = StringTokenizer(br.readLine())
+        val sum = (
+            token.nextToken().toInt() + 
+            token.nextToken().toInt()
+        ).toString()
+        bw.write(sum + "\n")
+    }
+    bw.flush()
+    bw.close()
 }
