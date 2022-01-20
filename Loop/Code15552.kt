4bfa2d039226872a4a4437fe74151b75d9d40a7b
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-20 21:38:38 +0900

    [Feature] - BufferedReader Example

---
+++ Loop/Code15552.kt
@@ -0,0 +1,8 @@
+import java.io.*;
+
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`));
+    val bw = BufferedWriter(OutputStreamWriter(System.out));
+    println(br);
+    println(bw);
+}
