854216a0277fc6e0c52404de2d11cc1eee445a25
GeonwooVincentKim <kdsnop@gmail.com>
2022-01-21 22:06:03 +0900

    [Feature] - Get the number as GCD-Algorithm first

---
+++ Loop/Code1934.kt
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+fun main(){
+    var sc: Scanner = Scanner(System.`in`);
+    var a = sc.nextInt();
+
+    for(i in 1..a){
+        var b = sc.nextInt();
+        var c = sc.nextInt();
+    
+        var r = 2;
+        while(r != 0) {
+            r = b % c;
+            b = c;
+            c = r;
+        }
+        
+        println(b);
+        // println(a * r);
+    }
+}
